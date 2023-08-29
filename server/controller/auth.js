c7c1560b5be5bc346feb825c88cf19956351479f
vaishnavishahakar <vaishnavishahakar1@gmail.com>
2023-08-30 00:51:52 +0530

    first commit

---
+++ server/controller/auth.js
@@ -0,0 +1,155 @@
+const { toTitleCase, validateEmail } = require("../config/function");
+const bcrypt = require("bcryptjs");
+const userModel = require("../models/users");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../config/keys");
+
+class Auth {
+  async isAdmin(req, res) {
+    let { loggedInUserId } = req.body;
+    try {
+      let loggedInUserRole = await userModel.findById(loggedInUserId);
+      res.json({ role: loggedInUserRole.userRole });
+    } catch {
+      res.status(404);
+    }
+  }
+
+  async allUser(req, res) {
+    try {
+      let allUser = await userModel.find({});
+      res.json({ users: allUser });
+    } catch {
+      res.status(404);
+    }
+  }
+
+  /* User Registration/Signup controller  */
+  async postSignup(req, res) {
+    console.log(req.body);
+    var emailId = req.body.email.toLowerCase();
+    let { name, email, password, cPassword } = req.body;
+  
+    let error = {};
+    if (!name || !email || !password || !cPassword) {
+      error = {
+        ...error,
+        name: "Filed must not be empty",
+        email: "Filed must not be empty",
+        password: "Filed must not be empty",
+        cPassword: "Filed must not be empty",
+      };
+      return res.json({ error });
+    }
+    if (name.length < 3 || name.length > 25) {
+      error = { ...error, name: "Name must be 3-25 charecter" };
+      return res.json({ error });
+    } else {
+      if (validateEmail(emailId)) {
+        name = toTitleCase(name);
+        if ((password.length > 255) | (password.length < 8)) {
+          error = {
+            ...error,
+            password: "Password must be 8 charecter",
+            name: "",
+            email: "",
+          };
+          return res.json({ error });
+        } else {
+          // If Email & Number exists in Database then:
+          try {
+            password = bcrypt.hashSync(password, 10);
+            email = req.body.email.toLowerCase();
+            const data = await userModel.findOne({ email: emailId });
+            if (data) {
+              error = {
+                ...error,
+                password: "",
+                name: "",
+                email: "Email already exists",
+              };
+              return res.json({ error });
+            } else {
+              console.log('new user')
+              let newUser = new userModel({
+                name,
+                email,
+                password,
+                // ========= Here role 1 for admin signup role 0 for customer signup =========
+                userRole: 1, // Field Name change to userRole from role
+              });
+              newUser
+                .save()
+                .then((data) => {
+                  const userData = { userName: data.name, userEmail: data.email}
+                  return res.json({
+                    success: 1,
+                    message: "Account create successfully. Please login",
+                    user: userData
+                  });
+                })
+                .catch((err) => {
+                  console.log(err);
+                });
+            }
+          } catch (err) {
+            console.log(err);
+          }
+        }
+      } else {
+        error = {
+          ...error,
+          password: "",
+          name: "",
+          email: "Email is not valid",
+        };
+        return res.json({ error });
+      }
+    }
+  }
+
+  /* User Login/Signin controller  */
+  async postSignin(req, res) {
+    let { email, password } = req.body;
+    if (!email || !password) {
+      return res.json({
+        error: "Fields must not be empty",
+      });
+    }
+    try {
+      var emailAddress = req.body.email.toLowerCase()
+      const data = await userModel.findOne({ email: emailAddress });
+      if (!data) {
+        return res.json({
+          message: 'Invalid Email Address, Please Enter Valid Email Address.',
+          success: 0
+        });
+      } else {
+        const login = await bcrypt.compare(password, data.password);
+        if (login) {
+          const token = jwt.sign(
+            { _id: data._id, role: data.userRole , email: data.email, phoneNumber: data.phoneNumber, verify: data.verify},
+            JWT_SECRET
+          );
+          const encode = jwt.verify(token, JWT_SECRET);
+          return res.json({
+            token: token,
+            user: encode,
+            message: 'Login Sucessfull',
+            success: 1
+          });
+        } else {
+          return res.json({
+            message: "Invalid Password, Please Enter Valid Password.",
+            success: 0
+          });
+        }
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
+
+const authController = new Auth();
+module.exports = authController;
---
+++ server/routes/users.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const usersController = require("../controller/users");
+
+router.get("/all-user", usersController.getAllUser);
+router.post("/single-user", usersController.getSingleUser);
+
+router.post("/add-user", usersController.postAddUser);
+router.post("/edit-user", usersController.postEditUser);
+router.post("/delete-user", usersController.getDeleteUser);
+
+router.post("/change-password", usersController.changePassword);
+
+module.exports = router;
